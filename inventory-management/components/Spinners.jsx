d7866e617da6bde4a4f587b737ca432c190bf5f4
Festus <fosayi@myseneca.ca>
2024-10-05 22:05:39 -0400

    Revert "Added some of the projects I have been working on lately"
    This reverts commit 4445c4e3c2c08fb5bbdffc9660697fb7911d40b9.

--- inventory-management/components/Spinners.jsx
+++
@@ -1,8 +0,0 @@
-import { PropagateLoader } from "react-spinners";
-export default function Spinners() {
-  return (
-    <div className="flex justify-center">
-      <PropagateLoader color={"#36d7b7"} speedMultiplier={2} size={30} />
-    </div>
-  );
-}
